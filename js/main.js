28e84ddd22053aea42b7eaba3f16519bb5c5cd1f
hwkww <c.kaik7@gmail.com>
2022-09-19 21:18:05 -0300

    :

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var titulo = document.querySelector(".titulo");
+var titulo2 = document.querySelector(".titulo2");
+
+titulo.textContent = "Aparecida Nutricionista";
+titulo2.textContent = "Pacientes:";

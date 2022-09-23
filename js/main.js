415d6ed7be678f7809a0f3f051c43eda52b4e507
hwkww <c.kaik7@gmail.com>
2022-09-23 14:28:54 -0300

    imc

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 var titulo = document.querySelector(".titulo");
-var titulo2 = document.querySelector(".titulo2");
-
 titulo.textContent = "Aparecida Nutricionista";
+var titulo2 = document.querySelector(".titulo2");
 titulo2.textContent = "Pacientes:";
+
+var paciente = document.querySelector("#Paulo");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent
+
+var imc = peso / (altura * altura);
+
+console.log(imc)
